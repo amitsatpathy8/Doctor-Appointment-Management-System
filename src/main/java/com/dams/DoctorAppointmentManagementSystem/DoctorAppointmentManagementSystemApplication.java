2ab814d12e1af82f7fdc84437b250bd7de529ca0
Amit Satpathy <amitsatpathy8@gmail.com>
2023-09-01 16:37:00 +0530

    project created
    Spring boot project structure is created

---
+++ src/main/java/com/dams/DoctorAppointmentManagementSystem/DoctorAppointmentManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.dams.DoctorAppointmentManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorAppointmentManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorAppointmentManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dams/DoctorAppointmentManagementSystem/DoctorAppointmentManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dams.DoctorAppointmentManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorAppointmentManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
